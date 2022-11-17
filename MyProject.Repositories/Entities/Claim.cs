0bd29cc5395ee0db14dceb9fb85198e68220f09d
student <student@541474mb.local>
2022-11-17 12:28:31 +0200

    initial commit

---
+++ MyProject.Repositories/Entities/Claim.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject.Repositories.Entities
+{
+    public enum Epolicy { Allow , Deny}
+    public class Claim
+    {
+        public string Id { get; set; }
+
+        public string RoleId { get; set; }
+
+        public string PermissionId { get; set; }
+
+        public Epolicy Policy { get; set; }
+    }
+}
